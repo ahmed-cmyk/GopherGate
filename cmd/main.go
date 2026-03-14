81e4084cc2d07efe5376f8eec5acbb923a4f6ce0
Ahmed Ikram <ikramahmed398@gmail.com>
2026-03-14 16:07:39 +0500

    feat: created basic implementation of a http server

---
+++ cmd/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, World!"))
+	})
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("Failed to start server")
+	}
+}
