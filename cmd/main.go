471c3fa3bef33c69fbc40765959cb830f66cd20e
Ahmed Ikram <ikramahmed398@gmail.com>
2026-03-14 19:11:10 +0500

    feat: updated server to handle shutdowns gracefully

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,22 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/ahmed-cmyk/GopherGate/internal/config"
 )
 
 func main() {
+	// Create a context that is cancelled when SIGINT or SIGNTERM is received
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
 	var config config.Config
 
 	err := config.LoadData("config.yaml")
@@ -18,13 +25,21 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Printf("Starting service: %s\n", config.ServiceName)
-	fmt.Printf("Listening on port %s\n", config.Server.Port)
+	// Run server inside a goroutine so that it doesn't block
+	go func() {
+		port := fmt.Sprintf(":%s", config.Server.Port)
+		err := http.ListenAndServe(port, nil)
+		if err != nil {
+			log.Fatalf("Failed to start server: %v\n", err)
+		}
+	}()
 
-	port := fmt.Sprintf(":%s", config.Server.Port)
+	log.Printf("Starting service: %s\n", config.ServiceName)
+	log.Printf("Listening on port %s\n", config.Server.Port)
 
-	err = http.ListenAndServe(port, nil)
-	if err != nil {
-		fmt.Printf("Failed to start server: %v\n", err)
-	}
+	// Wait for the interrupt signal
+	<-ctx.Done()
+
+	log.Println("Shutting down server gracefully...")
+	log.Println("Server gracefully stopped")
 }
