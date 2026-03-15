a228574b2102c02f58e4bb106fa7eec7384be617
Ahmed Ikram <ikramahmed398@gmail.com>
2026-03-15 10:22:53 +0500

    feat: updated logger format

--- internal/middleware/logging.go
+++ internal/middleware/logging.go
@@ -28,6 +28,6 @@ func Logging(next http.Handler) http.Handler {
 
 		elapsed := time.Since(start)
 
-		log.Printf("[%d] %s %s (Took %s)", rw.statusCode, r.Method, r.URL.Path, elapsed)
+		log.Printf("[%d] %s %s (Took %d ms)", rw.statusCode, r.Method, r.URL.Path, elapsed.Milliseconds())
 	})
 }
